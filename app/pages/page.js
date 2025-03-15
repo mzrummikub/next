eee177e10ff4a695df870ad56ecd4694a6f8d59a
MZRummikub <mzrummikub@gmail.com>
2025-03-15 19:17:10 +0100

    pages

---
+++ app/pages/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">Witaj w mojej aplikacji Next.js!</h1>
+        <p className="mt-4 text-gray-700">To jest początkowa strona.</p>
+      </div>
+    );
+  }
+  
