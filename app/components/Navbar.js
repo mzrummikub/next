1b0e6f30dda6dc98f7e2a37e0400ebb3cc031426
MZRummikub <mzrummikub@gmail.com>
2025-03-22 00:37:21 +0100

    22.03

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -76,7 +76,7 @@ export function Navbar() {
 
           <button
             type="submit"
-            className="px-3 py-1 bg-green-600 text-white rounded"
+            className="px-3 py-1 bg-blue-600 text-white rounded"
           >
             Zaloguj
           </button>
